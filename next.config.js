f1cdf0e7136a70b9739a0e2972ca1329460a09e6
Yuriy Boev <yuriy.boev@osf-commerce.com>
2022-01-24 16:10:02 +0200

    Added i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const nextTranslate = require('next-translate')
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en', 'de'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en',
+  },
+  trailingSlash: false,
+  swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = nextTranslate(nextConfig)
